0016c9e8fdda88882758547e8d3270879c78e7d0
Phantom <70034285+phantom2152@users.noreply.github.com>
2026-03-09 17:00:12 +0000

    seaparate out CF and non CF

--- src/index-cf.ts
+++ src/index-cf.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/bun";
 import YTSSearch from "./providers/yts";
 import TPBSearch from "./providers/piratebay";
 import type { TPBCategories } from "./providers/piratebay";
 
-const app = new Hono();
+type Bindings = {
+  ASSETS: {
+    fetch: typeof fetch;
+  };
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
 
 app.get("/api/search/yts", async (c) => {
   const query = c.req.query("query") || "";
@@ -21,23 +27,21 @@ app.get("/api/search/tpb", async (c) => {
   const category = c.req.query("category") || "All";
   const sortBy = c.req.query("sort") as 'name' | 'date' | 'size' | 'seeders' | 'leechers';
   const order = c.req.query("order") as 'asc' | 'desc';
-
+  
   if (!query) {
     return c.json({ status: "error", error: "Missing query parameter" }, 400);
   }
 
-  const results = await TPBSearch(query, {
+  const results = await TPBSearch(query, { 
     category: category as keyof TPBCategories,
     sortBy,
-    order,
-  });
+    order 
+  }); 
   return c.json(results);
 });
 
-// Serve static files from the "public" folder
-app.use("*", serveStatic({ root: "./src/frontend" }));
+app.get("*", async (c) => {
+  return c.env.ASSETS.fetch(c.req.raw);
+});
 
-export default {
-  port: process.env.PORT || 3000,
-  fetch: app.fetch,
-};
+export default app;
